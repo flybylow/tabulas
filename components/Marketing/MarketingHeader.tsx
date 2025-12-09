6a51918b5f0948b8cb90cf2f21e93662dc0c3f2c
Ward <ward@example.com>
2025-12-09 16:31:03 +0100

    Remove GitHub link from top navigation header
    - Removed GitHub button from MarketingHeader component
    - Header now only shows Tabulas logo

--- components/Marketing/MarketingHeader.tsx
+++ components/Marketing/MarketingHeader.tsx
@@ -1,8 +1,6 @@
 import clsx from "clsx";
 import Link from "next/link";
 import { ComponentProps } from "react";
-import { GitHubIcon } from "@/icons";
-import { LinkButton } from "@/primitives/Button";
 import { Container } from "@/primitives/Container";
 import { Logo } from "../Logo";
 import styles from "./MarketingHeader.module.css";
@@ -17,15 +15,6 @@ export function MarketingHeader({
         <Link href="/">
           <Logo />
         </Link>
-        <LinkButton
-          href="https://github.com/flybylow/tabulas"
-          icon={<GitHubIcon />}
-          target="_blank"
-          rel="noopener noreferrer"
-          variant="secondary"
-        >
-          View on GitHub
-        </LinkButton>
       </Container>
     </header>
   );
